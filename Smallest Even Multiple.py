97bf658587e04dfb92f6b248724e2c60214d2449
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2022-10-12 18:54:51 +0530

    Create Smallest Even Multiple.py
    Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
     
    Example 1:
    
    Input: n = 5
    Output: 10
    Explanation: The smallest multiple of both 5 and 2 is 10.
    Example 2:
    
    Input: n = 6
    Output: 6
    Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.

---
+++ Smallest Even Multiple.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n%2 ==0:
+            return n
+        else:
+            return n*2
+          
+          
+#### Runtime: 52 ms, faster than 58.64% of Python3 online submissions for Smallest Even Multiple.
+#### Memory Usage: 13.8 MB, less than 95.08% of Python3 online submissions for Smallest Even Multiple.
+        
